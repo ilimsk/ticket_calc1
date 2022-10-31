0ca088642370ebd183418a803171030a368e9ff9
ilimsk <113999486+ilimsk@users.noreply.github.com>
2022-10-31 17:27:57 +0700

    Update ticket_calc.py
    edit code

--- ticket_calc.py
+++ ticket_calc.py
@@ -1,19 +1,21 @@
 
 while True:
-    z = []
-    price = None
+    result = []
+    price = 0
+    guest = 0
     sum_ticket = int(input("Введите количество билетов: "))
     for count in range(sum_ticket):
-        age = int(input("Введите возраст: "))
+        guest += 1
+        age = int(input(f"Введите возраст {guest}-го гостя: "))
         if age <= 18:
-            z.append(0)
+            result.append(0)
         elif age > 18 and age < 25:
-            z.append(990)
+            result.append(990)
         elif age >= 25:
-            z.append(1390)
+            result.append(1390)
+    price = int(sum(result))
     if sum_ticket >= 3:
-        price = int(sum(z))-(int(sum(z))*0,1)
-        print(f"\nЦена билетов co скидкой: {price} рублей")
+        price_discont = price - (price*0.1)
+        print(f"\nЦена билетов co скидкой: {price_discont} рублей\n")
     else:
-        price = sum(z)
-        print(f"\nЦена билетов без скидки: {price} рублей")
+        print(f"\nЦена билетов без скидки: {price} рублей\n")
